f3aebf6e53cf652b02ff3ca660d4846ec06698dd
Irina Vorobjeva <i.s.vorobjeva@yandex.ru>
2021-01-22 19:31:40 +0500

    update

--- js/scripts.js
+++ js/scripts.js
@@ -23,7 +23,7 @@ jQuery("document").ready(function(s){
 		button.preventDefault();
 		var form = jQuery(this);
 		jQuery.ajax({
-			url: '../send.php',
+			url: './send.php',
 			type: 'POST',
 			data: form.serialize(),
 			success: function( data ) {
